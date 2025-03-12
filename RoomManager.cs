a90a3460ec5d38623f8a6bba8852325598053c73
Korrey136 <sekorrey.thomas@cuw.edu>
2025-03-11 23:45:04 -0500

    Update RoomManager.cs

--- RoomManager.cs
+++ RoomManager.cs
@@ -1,5 +1,78 @@
 using UnityEngine;
+using System.Collections.Generic;
 
+// Core class with a static Player variable
+public static class Core
+{
+    public static Player thePlayer;
+}
+
+// Player class
+public class Player
+{
+    private string name;
+    private Room currentRoom;
+
+    public Player(string name)
+    {
+        this.name = name;
+    }
+
+    public void SetCurrentRoom(Room room)
+    {
+        this.currentRoom = room;
+    }
+
+    public Room GetCurrentRoom()
+    {
+        return currentRoom;
+    }
+}
+
+// Dungeon class (basic structure)
+public class Dungeon
+{
+    // Add your dungeon logic here
+}
+
+// Room class
+public class Room
+{
+    private Dictionary<string, Room> exits = new Dictionary<string, Room>();
+
+    public Room() { }
+
+    public void SetExit(string direction, Room neighbor)
+    {
+        exits[direction] = neighbor;
+    }
+
+    public bool HasExit(string direction)
+    {
+        return exits.ContainsKey(direction);
+    }
+
+    public Room GetExit(string direction)
+    {
+        if (exits.ContainsKey(direction))
+        {
+            return exits[direction];
+        }
+        return null;
+    }
+
+    public bool TryToTakeExit(string direction)
+    {
+        if (exits.ContainsKey(direction))
+        {
+            Core.thePlayer.SetCurrentRoom(exits[direction]);
+            return true;
+        }
+        return false;
+    }
+}
+
+// RoomManager class
 public class RoomManager : MonoBehaviour
 {
     public GameObject[] theDoors;
@@ -10,46 +83,65 @@ public class RoomManager : MonoBehaviour
     {
         Core.thePlayer = new Player("Mike");
         this.theDungeon = new Dungeon();
-        this.setupRoom();
+        SetupRoom();
     }
 
-    //disable all doors
-    private void resetRoom()
+    // Disable all doors
+    private void ResetRoom()
     {
-        this.theDoors[0].SetActive(false);
-        this.theDoors[1].SetActive(false);
-        this.theDoors[2].SetActive(false);
-        this.theDoors[3].SetActive(false);
+        foreach (GameObject door in theDoors)
+        {
+            door.SetActive(false);
+        }
     }
 
-    //show the doors appropriate to the current room
-    private void setupRoom()
+    // Show the doors appropriate to the current room
+    private void SetupRoom()
     {
-        Room currentRoom = Core.thePlayer.getCurrentRoom();
-        this.theDoors[0].SetActive(currentRoom.hasExit("north"));
-        this.theDoors[1].SetActive(currentRoom.hasExit("south"));
-        this.theDoors[2].SetActive(currentRoom.hasExit("east"));
-        this.theDoors[3].SetActive(currentRoom.hasExit("west"));
+        Room currentRoom = Core.thePlayer.GetCurrentRoom();
+        theDoors[0].SetActive(currentRoom.HasExit("north"));
+        theDoors[1].SetActive(currentRoom.HasExit("south"));
+        theDoors[2].SetActive(currentRoom.HasExit("east"));
+        theDoors[3].SetActive(currentRoom.HasExit("west"));
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.UpArrow))
+        if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            //try to goto the north
+            // Try to go to the north
+            if (Core.thePlayer.GetCurrentRoom().TryToTakeExit("north"))
+            {
+                SetupRoom();
+            }
         }
-        else if(Input.GetKeyDown(KeyCode.LeftArrow))
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            //try to goto the west
+            // Try to go to the west
+            if (Core.thePlayer.GetCurrentRoom().TryToTakeExit("west"))
+            {
+                SetupRoom();
+            }
         }
-        else if(Input.GetKeyDown(KeyCode.RightArrow))
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
         {
-            //try to goto the east
+            // Try to go to the east
+            if (Core.thePlayer.GetCurrentRoom().TryToTakeExit("east"))
+            {
+                SetupRoom();
+            }
         }
-        else if(Input.GetKeyDown(KeyCode.DownArrow))
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
-            //try to goto the south
+            // Try to go to the south
+            if (Core.thePlayer.GetCurrentRoom().TryToTakeExit("south"))
+            {
+                SetupRoom();
+            }
         }
     }
 }
+
+
+   
